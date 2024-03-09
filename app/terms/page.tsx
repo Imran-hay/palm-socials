c6a5d9c29855e2b2c00c255282d223eebd30df7e
emran <imranhayredin89@gmail.com>
2024-03-09 17:19:54 +0300

    trial20

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,48 +1,46 @@
-"use client"
-import React, { useState,createContext, useContext } from 'react';
+import React from 'react';
 
 const TermsOfService = () => {
   return (
-      <div className=" flex flex-col ml-5">
-        <h1 className="text-3xl text-center mb-6 text-teal-700">Terms of Service</h1>
-        <p className="text-lg mb-4 text-cyan-600 text-center">
-          By using our app, you agree to the following Terms of Service:
-        </p>
-        <p className="text-lg mb-4 text-yellow-300">
-          1. You must comply with all applicable laws and regulations when using our app.
-        </p>
-        <p className="text-lg mb-4 text-yellow-300">
-          2. You are responsible for maintaining the confidentiality of your account credentials.
-        </p>
-        <p className="text-lg mb-4 text-yellow-300">
-          3. You may not use the app for any illegal or unauthorized purpose.
-        </p>
-        <p className="text-lg mb-4 text-yellow-300">
-          4. You are solely responsible for the content you publish using our app.
-        </p>
-        <p className="text-lg mb-4 text-yellow-300">
-          5. We reserve the right to suspend or terminate your access to the app at any time.
-        </p>
-        <p className="text-lg mb-4 text-yellow-300">
-          6. The app may include third-party content and links to external websites. We are not responsible for the accuracy or availability of such content.
-        </p>
-        <p className="text-lg mb-4 text-yellow-300">
-          7. We reserve the right to modify or discontinue the app at any time without prior notice.
-        </p>
-        <p className="text-lg mb-4 text-yellow-300">
-          8. The app is provided "as is" without any warranties, express or implied.
-        </p>
-        <p className="text-lg mb-4 text-yellow-300">
-          9. We are not liable for any damages or losses resulting from your use of the app.
-        </p>
-        <p className="text-lg mb-4 text-yellow-300">
-          10. These Terms of Service may be updated from time to time. It is your responsibility to review the latest version.
-        </p>
-        <p className="text-lg text-green-300 text-center">
-          If you have any questions or concerns about these Terms of Service, please contact us using minatech04@gmail.com.
-        </p>
-      </div>
-  
+    <div className="flex flex-col ml-5">
+      <h1 className="text-3xl text-center mb-6 text-teal-700">Terms of Service</h1>
+      <p className="text-lg mb-4 text-cyan-600 text-center">
+        By using our app, you agree to the following Terms of Service:
+      </p>
+      <p className="text-lg mb-4 text-yellow-300">
+        1. You must comply with all applicable laws and regulations when using our app.
+      </p>
+      <p className="text-lg mb-4 text-yellow-300">
+        2. You are responsible for maintaining the confidentiality of your account credentials.
+      </p>
+      <p className="text-lg mb-4 text-yellow-300">
+        3. You may not use the app for any illegal or unauthorized purpose.
+      </p>
+      <p className="text-lg mb-4 text-yellow-300">
+        4. You are solely responsible for the content you publish using our app.
+      </p>
+      <p className="text-lg mb-4 text-yellow-300">
+        5. We reserve the right to suspend or terminate your access to the app at any time.
+      </p>
+      <p className="text-lg mb-4 text-yellow-300">
+        6. The app may include third-party content and links to external websites. We are not responsible for the accuracy or availability of such content.
+      </p>
+      <p className="text-lg mb-4 text-yellow-300">
+        7. We reserve the right to modify or discontinue the app at any time without prior notice.
+      </p>
+      <p className="text-lg mb-4 text-yellow-300">
+        8. The app is provided &quot;as is&quot; without any warranties, express or implied.
+      </p>
+      <p className="text-lg mb-4 text-yellow-300">
+        9. We are not liable for any damages or losses resulting from your use of the app.
+      </p>
+      <p className="text-lg mb-4 text-yellow-300">
+        10. These Terms of Service may be updated from time to time. It is your responsibility to review the latest version.
+      </p>
+      <p className="text-lg text-green-300 text-center">
+        If you have any questions or concerns about these Terms of Service, please contact us using minatech04@gmail.com.
+      </p>
+    </div>
   );
 };
 
