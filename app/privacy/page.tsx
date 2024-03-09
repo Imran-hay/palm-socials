18ce704ead4ebbdeb71d6dd269e1e9b72cb4e5a9
emran <imranhayredin89@gmail.com>
2024-03-09 17:06:46 +0300

    terms and policies

---
+++ app/privacy/page.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+
+const PrivacyPolicy = () => {
+  return (
+
+      <div className=" flex flex-col ml-5">
+        <h1 className="text-3xl text-center mb-6 text-cyan-700">Privacy Policy</h1>
+        <h3 className="text-lg mb-4 text-yellow-500 w-full text-center ">
+          Our commitment to privacy:
+       
+        </h3>
+        <p className='text-cyan-200 mb-3'>
+        We respect your privacy and are committed to protecting your personal information. This Privacy Policy explains how we collect, use, and safeguard your information when you use our social media management app. By using our app, you consent to the practices described in this policy.
+
+        </p>
+        <h3 className="text-lg mb-4 text-yellow-500 w-full text-center ">
+          1. Information we collect:
+       
+        </h3>
+        <p className='text-cyan-200 mb-3'>
+       <p className='mb-4 text-cyan-700'> We may collect certain information from you when you use our app, including: </p>
+
+<p className='mb-2'>Personal information: such as your name, email address,phone number, and social media account credentials.</p>
+<p className='mb-2'>Usage information: such as your interactions with the app, posts viewed, analytics data, and preferences.</p>
+<p className='mb-2'>Device information: such as your device type, operating system, and unique device identifiers.</p>
+<p className='mb-2'>Log data: such as IP addresses, browser type, and pages visited.</p>
+        </p>
+        <h3 className="text-lg mb-4 text-yellow-500 w-full text-center ">
+          2. How we use your information:
+       
+        </h3>
+
+        <p className='text-cyan-200 mb-3'>
+        <p className='mb-4 text-cyan-700'> We use your information to provide and improve our app, including: </p>
+
+<p className='mb-2'>Managing and analyzing your social media accounts.</p>
+<p className='mb-2'>Sending important updates, notifications, and account-related information.</p>
+<p className='mb-2'>Enhancing app functionality, performance, and security.</p>
+<p className='mb-2'>Conducting research, analytics, and improving user experience.</p>
+
+        </p>
+        <h3 className="text-lg mb-4 text-yellow-500 w-full text-center ">
+          3. Information sharing:
+       
+        </h3>
+        <p className='text-cyan-200 mb-3'>
+        <p className='mb-4 text-cyan-700'> We may share your information with third parties under the following circumstances: </p>
+
+<p className='mb-2'>With your consent or as necessary to fulfill the purpose for which you provided the information.</p>
+<p className='mb-2'>With service providers who assist us in app operations, analytics, or other business-related activities.</p>
+<p className='mb-2'>In response to a legal request or to protect our rights, property, or safety.</p>
+
+
+        </p>
+        <h3 className="text-lg mb-4 text-yellow-500 w-full text-center ">
+          4. Data Security:
+       
+        </h3>
+
+        <p className='text-cyan-200 mb-3'>
+        We implement industry-standard security measures to protect your information from unauthorized access, disclosure, or alteration. However, please note that no method of transmission over the internet or electronic storage is completely secure.
+
+        </p>
+        <h3 className="text-lg mb-4 text-yellow-500 w-full text-center ">
+          5 Your choices and rights:
+       
+        </h3>
+
+        <p className='text-cyan-200 mb-3'>
+        You have the right to access, update, correct, or delete your personal information. You may also opt-out of receiving promotional communications from us. Please contact us using the information provided below to exercise your rights.
+
+        </p>
+      
+        <h3 className="text-lg mb-4 text-yellow-500 w-full text-center ">
+          6 Changes to this policy:
+       
+        </h3>
+        <p className='text-cyan-200 mb-3'>
+        We may update this Privacy Policy from time to time. We will notify you of any material changes by posting the updated policy on our website or through other communication channels.
+
+        </p>
+        <h3 className="text-lg mb-4 text-yellow-500 w-full text-center ">
+          7 Contact us:
+       
+        </h3>
+
+        <p className='text-cyan-200 mb-3'>
+        If you have any questions, concerns, or requests regarding this Privacy Policy or our data practices, please contact us at minatech04@gmail.com.
+
+        </p>
+
+        <h3 className='text-center text-green-400 mt-5 text-lg'>By using our app, you acknowledge that you have read and understood this Privacy Policy.</h3>
+      </div>
+    
+  );
+};
+
+export default PrivacyPolicy;
